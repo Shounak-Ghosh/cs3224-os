71984c2be65bb72d9d1c852c55644e907e2ba0f0
Shounak Ghosh <sg7569@nyu.edu>
2024-10-25 09:53:56 -0400

    removed find bday method, added cli

--- hw7/lab7_1.c
+++ hw7/lab7_1.c
@@ -12,31 +12,29 @@
 pthread_mutex_t mutex;
 int nhits = 0; // Shared variable to count successful trials
 
-int hasDuplicateBirthday(int *birthdays, int n) {
-    int found[365] = {0}; // Array to track birthdays
-    for (int i = 0; i < n; ++i) {
-        if (found[birthdays[i]]) {
-            return 1; // Found a duplicate
-        }
-        found[birthdays[i]] = 1;
-    }
-    return 0;
-}
-
+// param is a pointer to an int representing the number of students in the class
+// void* is a generic pointer type in C
 void* WorkerThread(void* param) {
     int n = *(int*)param;
     int local_hits = 0;
     int birthdays[n];
     
     // Seed the random number generator uniquely for each thread
-    unsigned int rand_state = (unsigned int)time(NULL) + (unsigned int)pthread_self();
+    unsigned int rand_state = (unsigned int)time(NULL) + pthread_self();
     
     for (int i = 0; i < NUM_TRIALS; ++i) {
         for (int j = 0; j < n; ++j) {
+            // generate birthdays
             birthdays[j] = rand_r(&rand_state) % 365;
         }
-        if (hasDuplicateBirthday(birthdays, n)) {
-            ++local_hits;
+        // check collisions
+        int found[365] = {0}; // Array to track birthdays
+        for (int i = 0; i < n; ++i) {
+            if (found[birthdays[i]]) {
+                ++local_hits; // Found a duplicate
+                break;
+            }
+            found[birthdays[i]] = 1;
         }
     }
 
@@ -48,15 +46,19 @@ void* WorkerThread(void* param) {
     return NULL;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s n1 n2 ... nn\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     pthread_t threads[NUM_THREADS];
     pthread_mutex_init(&mutex, NULL);
 
-    int n_values[] = {49,23,41,128}; // Test with different class sizes
-
-    for (int k = 0; k < sizeof(n_values)/sizeof(n_values[0]); ++k) {
+    for (int k = 1; k < argc; ++k) {
         nhits = 0; // Reset nhits for each class size
-        int n = n_values[k];
+        int n = atoi(argv[k]); // convert argument to int
 
         // Create and start threads
         for (int i = 0; i < NUM_THREADS; ++i) {
@@ -69,9 +71,8 @@ int main() {
         }
 
         double probability = (double)nhits / (NUM_THREADS * NUM_TRIALS);
-        printf("Probability of at least two students sharing a birthday in a class of %d students: %.2f%%.\n", n, 100 * probability);
+        printf("Probability of at least two students sharing a birthday in a class of %d students: %.2f%%.\n", n, probability * 100);
     }
 
     pthread_mutex_destroy(&mutex);
-    return 0;
 }
